3c174df80f4e002e85a1c77dbea2bb9802a444a3
Tristan Wimmer <tristan.a.wimmer@gmail.com>
2018-04-08 18:01:52 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -4,3 +4,19 @@ function init() {
   // Write your JavaScript code inside the init() function
 
 }
+
+function onKeyDownHandler(e) {
+  const key = parseInt(e.detail || e.which);
+ 
+  if (key === alphabet[index]) {
+    index++;
+ 
+    if (index === alphabet.length) {
+      alert("Hurray!");
+ 
+      index = 0;
+    }
+  } else {
+    index = 0;
+  }
+}
