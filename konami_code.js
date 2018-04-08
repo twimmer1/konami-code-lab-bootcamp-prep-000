c58d0cbb512e1b2a02d015acecf389b564809bb2
Tristan Wimmer <tristan.a.wimmer@gmail.com>
2018-04-08 18:28:34 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -1,8 +1,8 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
-//function init() {
+function init() {
   document.body.addEventListener('keydown', onKeyDownHandler)
-//}
+}
 
 // Key codes for A, B, and C keys.
 const alphabet = [65, 66, 67];
